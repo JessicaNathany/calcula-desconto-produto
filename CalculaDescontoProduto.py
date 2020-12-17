2f57ac2b907e022ddebf91b211f9ef341efb4949
Jéssica Nathany Carvalho Freitas <jnathany@outlook.com>
2020-12-17 08:41:02 -0300

    ajustes

--- CalculaDescontoProduto.py
+++ CalculaDescontoProduto.py
@@ -1,14 +1,15 @@
 # Programa calcula o desconto do produto a prazo e á vista
 # Para pagamento á vista desconto é de 10% para pagamento parcelado, desconto de 5%
 
+nomeProduto = input('Digite o nome do produto: ')
 preco = float(input('Digite o preço do produto: R$ '))
 tipoPagamento = int(input('Escolha a forma de pagamento: digite 1 á vista ou 2 parcelar:'))
 
 if(tipoPagamento == 1):
     precoDesconto = preco - (preco * 5 / 100)
-    print('O preço do produto com 5% é de: R${:.2f}'.format(precoDesconto))
+    print('O preço do {} com 5% é de: R${:.2f}'.format(nomeProduto, precoDesconto))
 else:
     precoDesconto = preco - (preco * 10 / 100)
-    print('O preço do produto com 10% é de: R${:.2f}'.format(precoDesconto))
+    print('O preço do {} com 10% é de: R${:.2f}'.format(nomeProduto, precoDesconto))
 
 
